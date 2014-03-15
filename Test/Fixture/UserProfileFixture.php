462d47a0fbc18e512ce6528761fc0317eb9809e1
Jason Snider <root@jasonsnider.com>
2014-03-15 16:24:13 -0500

    Added the UserProfile fixture

---
+++ Test/Fixture/UserProfileFixture.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * UserProfileFixture
+ *
+ */
+class UserProfileFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36, 'key' => 'primary', 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'user_id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'display_name' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 60, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'full_name' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 60, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'first_name' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 20),
+		'last_name' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 20),
+		'about_me' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'created_user_id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified_user_id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => '532382da-e2a0-4e31-8424-cfc1cd9b91e0',
+			'user_id' => 'Lorem ipsum dolor sit amet',
+			'display_name' => 'Lorem ipsum dolor sit amet',
+			'full_name' => 'Lorem ipsum dolor sit amet',
+			'first_name' => 1,
+			'last_name' => 1,
+			'about_me' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'created' => '2014-03-14 17:29:46',
+			'created_user_id' => 'Lorem ipsum dolor sit amet',
+			'modified' => '2014-03-14 17:29:46',
+			'modified_user_id' => 'Lorem ipsum dolor sit amet'
+		),
+	);
+
+}
